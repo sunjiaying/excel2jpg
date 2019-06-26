1d6f1dd4872e4ccfaa8404b46cd44b752ea52ac5
sunjiaying <sunjoin@qq.com>
2019-06-26 16:02:43 +0800

    first commit

---
+++ README.txt
@@ -0,0 +1,13 @@
+# excel2jpg
+
+
+可以用yarn命令安装环境依赖
+
+参考
+
+https://github.com/bauhausjs/phantom-html2pdf
+
+https://github.com/vesteraas/pdftojpg
+
+需要独立安装ghostscript
+https://www.ghostscript.com/download/gsdnld.html
---
+++ main.js
@@ -0,0 +1,50 @@
+var pdf = require('phantom-html2pdf');
+var fs =require('fs');
+var XLSX = require('xlsx');
+var buf = fs.readFileSync("./test.xlsx");
+var wb = XLSX.read(buf, {type:'buffer'});
+var pdftojpg = require('pdftojpg');
+var path = require('path');
+var temp = require('temp').track();
+var assert = require('assert');
+
+var tab = wb.Sheets[wb.SheetNames[0]];
+// console.log(tab);
+// console.log(tab.A4.v);
+
+var pdfOptions = {
+  html: '<!DOCTYPE html><html lang="en"><body><h1>'+tab.A4.v+' sdfsdfs</h1></body></html>',
+  paperSize: {
+    format: 'A4',
+    orientation: 'landscape', // portrait
+    border: '1cm'
+  }
+};
+
+pdf.convert(pdfOptions, function(err, result) {
+
+	/* Using a buffer and callback */
+	result.toBuffer(function(returnedBuffer) {});
+
+	/* Using a readable stream */
+	var stream = result.toStream();
+
+	/* Using the temp file path */
+	var tmpPath = result.getTmpPath();
+
+	/* Using the file writer and callback */
+	result.toFile("./file.pdf", function() {
+    var testFile = path.join(__dirname, 'file.pdf');
+    temp.open({suffix: '.jpg'}, function(err, tempFile) {
+      var dest = path.join(__dirname, 'file.jpg');
+      pdftojpg.convert(testFile, dest, function (err) {
+        if (err) {
+          throw err;
+        } else {
+          var buffer = fs.readFileSync(dest);
+          assert(buffer[0] === 0xff && buffer[1] === 0xd8);
+        }
+      });
+    });
+  });
+});
